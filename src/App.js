36a6d90afb44ddedf29ae828ec43a87588353537
dvalvarez17 <danielvalenciaalvarez77@gmail.com>
2023-06-03 03:10:03 +0000

    Changing name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there Daniel Valencia Alvarez
+          Hi there Daniel Valencia A
         </p>
         <a
           className="App-link"
